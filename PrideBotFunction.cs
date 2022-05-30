6757a7bc1f934203247e1cf0b1a9ec25d6cc139e
Rachel Hutchinson <rachelhutchinson93@gmail.com>
2022-05-30 16:31:14 +0100

    Locally running version

---
+++ PrideBotFunction.cs
@@ -0,0 +1,38 @@
+/*
+ * LambdaSharp (λ#)
+ * Copyright (C) 2018-2022
+ * lambdasharp.net
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace PrideBot;
+
+using LambdaSharp;
+using LambdaSharp.Slack;
+
+public sealed class Function : ALambdaSlackCommandFunction {
+
+    //--- Methods ---
+    public override async Task InitializeAsync(LambdaConfig config) {
+
+        // TO-DO: add function initialization and reading configuration settings
+    }
+
+    protected override async Task ProcessSlackRequestAsync(SlackRequest request) {
+
+        // TO-DO: add business logic
+
+        Console.WriteLine("Hello world!");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace PrideBot // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
