03ab8534430d3c5a7f0e48a3a0006ef9d10d9ca7
Jasper <jasper.jackson@madetech.com>
2022-06-01 12:49:46 +0100

    didn't add all files

--- AWS_App/Program.cs
+++ AWS_App/Program.cs
@@ -4,15 +4,18 @@ using System.Net;
 using System.Text;
 using System.Collections.Specialized;
 using SlackApp;
+using MargieBot;
 
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 var slackbot = new Bot();
 slackbot.RespondsTo("Hi").With("A nice response");
-slackbot.Connect("");
+slackbot.Connect("xoxb-3533570877828-3584756602661-IEK2dchjNfdAiHnhEfz9zlVS");
   
      SlackClient client = new SlackClient(Configuration.WebhookUrl);  
   
+while (Console.ReadLine() != "close") { }
+
      client.PostMessage(username: "test-rachel",  
                 text: "THIS IS A TEST MESSAGE!!",  
                 channel: "#general");  
